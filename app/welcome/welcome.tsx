7824bb35f63bf6242d49097db056a99c296f2f4d
testing-xx <axelkurn@gmail.com>
2025-03-13 21:21:17 +0700

    Feat: Add more color variables

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,7 +3,7 @@ import logoLight from "./logo-light.svg";
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="flex items-center justify-center pt-16 pb-4 bg-[var(--primary)]">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
