68b0cdda43ca06d351599a3c26f4e3a307174dc0
testing-xx <axelkurn@gmail.com>
2025-03-14 10:03:37 +0700

    Feat: Add Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+
+
+    return <>
+        <div className="w-full bg-[var(--primary)] p-5">
+            <h2 className="font-semibold text-white text-3xl">CareerLink</h2>
+        </div>
+    </>
+}
