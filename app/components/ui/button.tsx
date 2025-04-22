9bd6ddd35aa1a64d19a3562ad81859eede1f56a0
hendrahartanto <hendrazhuo10@gmail.com>
2025-04-22 11:53:34 +0700

    fix: button component color

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-accent text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-accent text-primary-foreground shadow-xs hover:bg-accent/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/50 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
