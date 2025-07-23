1ab8228779fc36169abdc98c9d4cd5b0e013496c
hendrahartanto <hendrazhuo10@gmail.com>
2025-07-23 17:41:39 +0700

    fix: bootcamp detail image overflow

--- app/components/bootcamp/bootcamp-detail-card.tsx
+++ app/components/bootcamp/bootcamp-detail-card.tsx
@@ -28,24 +28,27 @@ const BootcampDetailCard = ({
 
   return (
     <>
-      <div className={"flex bg-white p-6 rounded-xl gap-16"}>
-        <img
-          src={`${import.meta.env.VITE_STORAGE_URL}/${image}`}
-          alt="bootcamp's image"
-          className="rounded-lg border"
-        />
-        <div className={"flex flex-col gap-2 justify-between flex-1"}>
-          <div className="flex flex-col gap-5">
-            <p className={"text-primary text-3xl font-semibold"}>{name}</p>
-            <div className="flex gap-2">
-              <BootcampTypeTag type={type.name} />
-              <BootcampMethodTag type={category.name} />
+      <div className="flex flex-col bg-white p-6 rounded-xl">
+        <div className={"flex gap-8"}>
+          <img
+            src={`${import.meta.env.VITE_STORAGE_URL}/${image}`}
+            alt="bootcamp's image"
+            className="rounded-lg border w-80 object-cover"
+          />
+          <div className={"flex flex-col gap-2 justify-between flex-1"}>
+            <div className="flex flex-col gap-5">
+              <p className={"text-primary text-3xl font-semibold"}>{name}</p>
+              <div className="flex gap-2">
+                <BootcampTypeTag type={type.name} />
+                <BootcampMethodTag type={category.name} />
+              </div>
+              <p className={"text-justify"}>{description}</p>
             </div>
-            <p className={"text-justify"}>{description}</p>
           </div>
-          <div className="flex justify-end gap-5">
-            {role == "admin" && (
-              <>
+        </div>
+        <div className="flex justify-end gap-5">
+          {role == "admin" && (
+            <>
               <Button className="w-fit p-5" onClick={onClick}>
                 Add Session
               </Button>
@@ -59,9 +62,8 @@ const BootcampDetailCard = ({
                   View Enrolled Students
                 </Button>
               </Link>
-              </>
-            )}
-          </div>
+            </>
+          )}
         </div>
       </div>
     </>
