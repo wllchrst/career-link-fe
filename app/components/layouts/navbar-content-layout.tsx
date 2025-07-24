3168e4dad4fa8b9f389870013c6d9c674475f211
hendrahartanto <hendrazhuo10@gmail.com>
2025-07-24 17:39:00 +0700

    feat: add optional subtitle for the header

--- app/components/layouts/navbar-content-layout.tsx
+++ app/components/layouts/navbar-content-layout.tsx
@@ -1,12 +1,16 @@
 interface Props {
   children: React.ReactNode;
   title?: string;
+  subtitle?: string;
 }
 
-export const NavbarContentLayout = ({ title, children }: Props) => {
+export const NavbarContentLayout = ({ title, children, subtitle }: Props) => {
   return (
     <div className="container mt-4">
-      <div className="text-2xl text-primary font-bold mb-6">{title}</div>
+      <div className="flex flex-col mb-6">
+        <div className="text-2xl text-foreground font-semibold">{title}</div>
+        <div className="text-muted-foreground text-sm">{subtitle}</div>
+      </div>
       {children}
     </div>
   );
