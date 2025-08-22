60f7eb5bd7297e66a86f751c2eab9baada741775
axelkrnwn <axelkurn@gmail.com>
2025-08-22 10:30:48 +0700

    Feat: Change icon and title

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
+        <title>Career Link</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>  
         <Links />
       </head>
       <body>
